eb298ff1c682b8bc84e88f461d35cc503055efdb
Nathan Seidle <nathan@sparkfun.com>
2017-02-06 12:02:13 -0700

    Expanding example code

--- MLX90393-Example/MLX90393-Example/MLX90393-Example.ino
+++ MLX90393-Example/MLX90393-Example/MLX90393-Example.ino
@@ -1,17 +1,17 @@
 /* 
- HTU21D Humidity Sensor Example Code
+ MLX90393 Magnetometer Example Code
  By: Nathan Seidle
  SparkFun Electronics
- Date: September 15th, 2013
+ Date: February 6th, 2017
  License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
  
- Get humidity and temperature from the HTU21D sensor.
+ Read the mag fields on three XYZ axis
  
  Hardware Connections (Breakoutboard to Arduino):
- -VCC = 3.3V
- -GND = GND
- -SDA = A4
- -SCL = A5
+ 3.3V = 3.3V
+ GND = GND
+ SDA = A4
+ SCL = A5
  
  Serial.print it out at 9600 baud to serial monitor.
  */
@@ -23,40 +23,30 @@
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("MLX90393 Example!");
+  Serial.println("MLX90393 Example");
 
   Wire.begin();
 }
 
 void loop()
 {
-  Serial.println("1");
-
   Wire.beginTransmission(MLX90393_ADDRESS);
-  Wire.write(0b01001000); //Read measurement, Z axis
+  Wire.write(0b01001000); //Read measurement command, Z axis
   Wire.endTransmission();
-
-  Serial.println("2");
 
   Wire.requestFrom(MLX90393_ADDRESS, 3);
 
-  Serial.println("3");
-
   int status = Wire.read();
-  int response2 = Wire.read();
-  int response3 = Wire.read();
+  byte zMSB = Wire.read();
+  byte zLSB = Wire.read();
+
+  int zReading = ((int)zMSB << 8) | zLSB;
     
   Serial.print("status: 0x");
   Serial.print(status, HEX);
-  Serial.print(" response2: ");
-  Serial.print(response2);
-  Serial.print(" response3: ");
-  Serial.print(response3);
+  Serial.print(" z: ");
+  Serial.print(zReading);
   Serial.println();
 
-  Serial.println("Stopped");
-  while(1) ;
+  delay(1000);
 }
-
-
-
